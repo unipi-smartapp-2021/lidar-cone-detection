ff5f43ec108aae4fa89b6795786b29761083b062
Simone <official.simone.rizzo@gmail.com>
2021-12-02 13:13:52 +0100

    Full workload completed. Created from .pcd to img and from img to .pcd

---
+++ roslidar_conversion_tool/img2pcd.py
@@ -0,0 +1,47 @@
+import argparse
+import sys
+
+import numpy as np
+from PIL import Image
+import open3d as o3d
+from utils import *
+
+
+def parse_arguments(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_folder', type=str, default="", help='the path where the images and metadata minmax files are stored')
+    parser.add_argument('--filename_name', type=str, default="", help='the name of the image with extension es img.png')
+    parser.add_argument('--output', type=str, default=".", help='path file where the pointcloud is going to be stored es ./pcds/cloud.pcd')
+    parser.add_argument('--visualize', type=bool, default=False, help='visualize the output in a 3D space')
+    opt = parser.parse_known_args()[0] if known else parser.parse_args()
+    return opt
+
+
+if __name__ == "__main__":
+    opt_parser = parse_arguments()
+    if len(sys.argv) <= 5:
+        print("Please insert the --input, --image_name and --output arguments")
+        exit(0)
+    img_file__path = opt_parser.image_folder
+    image_name = opt_parser.filename_name
+    output__path = opt_parser.output
+
+    """Read back the image"""
+    r, el, az = read_spherical_image(img_file__path+image_name)
+
+    r_minmax, az_minmax, el_minmax = read_minmax_file(img_file__path+(image_name.split(".")[0])+"_minmax.txt")
+
+    """Rescale back"""
+    r, _, = scale(r_minmax, r)
+    az, _, = scale(az_minmax, az)
+    el, _, = scale(el_minmax, el)
+
+    """Convert back to Cartesian"""
+    x, y, z = sph2cart(az, el, r) # convert back to cartesian
+
+    """Visualize the point cloud"""
+    cloud = np.column_stack((x, y, z))
+    save_numpy_as_pcd(cloud, output__path)
+    if opt_parser.visualize:
+        visualize_pcd_file(output__path)
+
---
+++ roslidar_conversion_tool/pcd2img.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+from utils import read_pcd_and_filter, cart2sph, scale, create_image, from_pcd_to_image
+
+
+def parse_arguments(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default="", help='the path to the .pcd file es cloud0.pcd')
+    parser.add_argument('--image_name', type=str, default="", help='the name of the image with extension es img.png')
+    parser.add_argument('--output', type=str, default=".", help='path where the images are going to be stored es ./img/')
+    opt = parser.parse_known_args()[0] if known else parser.parse_args()
+    return opt
+
+
+if __name__ == "__main__":
+    opt_parser = parse_arguments()
+    if len(sys.argv) <= 5:
+        print("Please insert the --input, --image_name and --output arguments")
+        exit(0)
+    pcd_file__path = opt_parser.input
+    image_name = opt_parser.image_name
+    output__path = opt_parser.output
+    from_pcd_to_image(pcd_file__path, output__path,image_name)
+
+

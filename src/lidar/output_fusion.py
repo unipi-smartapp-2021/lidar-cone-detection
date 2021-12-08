7b41e872c45a3371fb3cc3760aab7610146508c5
Simone <official.simone.rizzo@gmail.com>
2021-12-08 23:01:20 +0100

    TODO visualize the depthmap as pointcloud

--- src/lidar/output_fusion.py
+++ src/lidar/output_fusion.py
@@ -1,15 +1,16 @@
 import open3d as o3d
 from matplotlib import pyplot as plt
 import numpy as np
+from scipy.spatial import transform as trsf
+from utils import read_minmax_file
 
 colored = o3d.io.read_image("./stereo/rgb_camera.png")
 depth = o3d.io.read_image("./stereo/depth_camera.png")
 rgbdimage = o3d.geometry.RGBDImage.create_from_color_and_depth(colored, depth)
-plt.imshow(rgbdimage.color)
-# plt.imshow(rgbdimage.depth)
-# plt.show()
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbdimage, o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
 
+r_minmax, az_minmax, el_minmax = read_minmax_file("./stereo/minmax.txt")
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbdimage, o3d.camera.PinholeCameraIntrinsic(o3d.camera.PrimeSenseDefault))
+# pcd = trsf.rotation(pcd, [[1,0,0,0], [0,-1,0,0], [0,0,-1,0], [0,0,0,1]])
 # pcd.tranform([[1,0,0,0], [0,-1,0,0], [0,0,-1,0], [0,0,0,1]])
-print(pcd)
 o3d.visualization.draw_geometries([pcd])

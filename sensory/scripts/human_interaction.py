9187eaa4ad7fcdc7c6b811d57e3664b20eb355d4
Niko Dalla Noce <n.dallanoce@studenti.unipi.it>
2021-12-14 12:15:08 +0100

    human argument is now checked

--- sensory/scripts/human_interaction.py
+++ sensory/scripts/human_interaction.py
@@ -10,10 +10,10 @@ def check_message_validity(message: str):
     avail_comm=['start', 'stop']
     message = message.lower().strip()
     if message not in avail_comm:
+        #rospy.logfatal('The only allowed arguments are '+ str(avail_comm))
         raise ArgumentError('The only allowed arguments are '+ str(avail_comm))
     return message  
         
-
 
 def talker(message):
     pub = rospy.Publisher('/human_interaction', String, queue_size=1)
@@ -25,7 +25,10 @@ def talker(message):
 if __name__ == '__main__':
 
     try:
-        message = check_message_validity(sys.argv[1])
-        talker(message)
+        if len(sys.argv) > 1:
+            message = check_message_validity(sys.argv[1])
+            talker(message)
+        else:
+            rospy.logfatal("Provide a 'start' or 'stop' argument!")    
     except rospy.ROSInterruptException:
         pass
